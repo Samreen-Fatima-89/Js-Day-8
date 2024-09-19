97df97a892fb48b34bcee986f979b77d7a1e1584
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-19 13:37:37 +0500

    competed

---
+++ app.js
@@ -0,0 +1,12 @@
+// var animals = ["Dog", "Cat", "Cow", "Buffalo"];
+// var lastReomove = animals.pop(); // Return last deleted array item
+// var firstRemove = animals.shift(); // Return first deleted array item
+// var lastAdd = animals.push("Billa"); // Return new length of array
+// var firstAdd = animals.unshift("Billi"); // Return new length of array
+// var copy = animals.slice(1, 2); // Returns copy of part an array
+// var addRemove = animals.splice(0, 2, "Arfan"); // Returns deleted items from array
+// console.log(copy);
+
+var colors = ["red", "black", "green", "white"];
+colors.splice(0, 2, "pink", "brown");
+console.log(colors);
